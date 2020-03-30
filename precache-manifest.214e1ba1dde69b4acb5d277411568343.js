61b55b590ccab213f767f45af3046fae4e1e3359
Suraj Kumbhar <surajkannankumbhar@gmail.com>
2020-03-30 16:20:32 +0530

    Updates

---
+++ precache-manifest.214e1ba1dde69b4acb5d277411568343.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e456f5d02034c38673ba78aa5495a2e",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "5848e815fd42b8179b3e",
+    "url": "/todo-react/static/css/main.d12423a4.chunk.css"
+  },
+  {
+    "revision": "552aacc46288e38be98a",
+    "url": "/todo-react/static/js/2.0ff04ffd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-react/static/js/2.0ff04ffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5848e815fd42b8179b3e",
+    "url": "/todo-react/static/js/main.0fe83f5f.chunk.js"
+  },
+  {
+    "revision": "33a849e1635b81d51e67",
+    "url": "/todo-react/static/js/runtime-main.bbc1f86c.js"
+  }
+]);
